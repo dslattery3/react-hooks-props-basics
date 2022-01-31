919497fbefe973f5e17f710ed687a172b794bee7
Dalton <slattery.da@gmail.com>
2022-01-31 10:17:46 -0600

    commit tree 8b5c9cfc45f1b63c983ec78273a8754a6d15f0d3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import BlogPost from "./BlogPost";
+import BlogContent from "./BlogContent";
+import Comment from "./Comment";
 
 function App() {
-  return <BlogPost />;
+  return (
+  <>
+    <BlogPost />
+    <BlogContent />
+    <Comment />
+  </>
+  )
 }
 
 export default App;
